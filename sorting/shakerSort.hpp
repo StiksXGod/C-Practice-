b1b02a6ca532d0233c74d9a90bf2a8e0f0efe80f
StiksXGod <mudrov.pavel@bk.ru>
2023-11-10 22:27:39 +0300

    shaker Sort

---
+++ sorting/shakerSort.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+void shakerSort(int *mass, int size){
+    int low = 0, up = size-1;
+    int last;
+
+    while(low<up){
+        last = -1;
+        for (int i = low; i < up; i++)
+        {
+            if (mass[i]>mass[i+1])
+            {
+                int temp = mass[i];
+                mass[i] = mass[i+1];
+                mass[i+1] = temp;
+                last = i; 
+            }
+            
+        }
+        if (last==-1) break;
+        up = last;
+        
+        for (int i = up; i >= low; i--)
+        {
+            if (mass[i]>mass[i+1])
+            {
+                int temp = mass[i];
+                mass[i] = mass[i+1];
+                mass[i+1] = temp;
+                last = i; 
+            }
+        }
+        low = last+1;
+
+    }
+}
