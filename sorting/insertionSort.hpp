8dddea70fdf7f1a6b66c96bfcbb12529f773f69c
StiksXGod <mudrov.pavel@bk.ru>
2023-11-11 17:50:44 +0300

    insertion Sort

--- sorting/insertionSort.hpp
+++ sorting/insertionSort.hpp
@@ -2,23 +2,15 @@
 
 
 void insertionSort(int *arr, int n){
-    for (int i = 0; i < n-1; i++)
+    int element, j;
+    for (int i = 1; i < n; i++)
     {
-        int min = i;
-        for (int j = i+1; j < n; j++)
-        {
-            if (arr[j]<arr[min])
-            {
-                min = j;
-            }
-            
+        element = arr[i];
+        j = i;
+        while(j>0 and arr[j-1]>element){
+            arr[j] = arr[j-1];
+            j = j-1;
         }
-        if (min!=i)
-        {
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-        }
+        arr[j] = element;
     }
-    
 }
