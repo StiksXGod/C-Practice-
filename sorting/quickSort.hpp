d3a709980a3df269a69ad267b396730368f6f8e4
StiksXGod <mudrov.pavel@bk.ru>
2023-11-13 01:08:33 +0300

    Hoar sort

---
+++ sorting/quickSort.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void quickSort(int arr[], int left, int right){
+    if(left>right) return;
+    int pivot = arr[((left+right)/2)];
+    int i = left;
+    int j = right;
+    while(i<=j){
+        while(arr[i]<pivot) i++;
+        while(arr[j]>pivot) j--;
+        if (i<=j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--; 
+        }
+        
+    }
+    quickSort(arr,left,j);
+    quickSort(arr,i,right);
+    
+}
